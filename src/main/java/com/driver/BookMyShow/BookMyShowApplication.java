3b84169f4deada709fba6bb18af4484b509c4245
TheThunderB0lt <nikithkumarniki160698@gmail.com>
2023-12-26 22:17:11 +0530

    initial Commit

---
+++ src/main/java/com/driver/BookMyShow/BookMyShowApplication.java
@@ -0,0 +1,13 @@
+package com.driver.BookMyShow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyShowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMyShowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/driver/BookMyShow/BookMyShowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.driver.BookMyShow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyShowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
